55096adf1704b78860312e8581afe6cc539627f1
pierthodo <pierthodo@gmail.com>
2021-04-01 11:26:48 +0100

    add ray first script

--- scripts/ray_test_1.py
+++ scripts/ray_test_1.py
@@ -1,19 +1,48 @@
 
 import numpy as np
 import ray
+import gym
+import time
 
 @ray.remote
-class ParameterServer(object):
-    def __init__(self, dim):
-        # Alternatively, params could be a dictionary
-        # mapping keys to arrays.
-        self.params = np.zeros(dim)
+class StreamServer(object):
+    def __init__(self):
+        self.stream = {}
 
-    def get_params(self):
-        return self.params
-    def update_params(self, grad):
-        self.params += grad
+    def create_stream(self,data_list):
+        for key,data in data_list:
+            self.stream[key] = [data]
+
+    def get(self,key_list):
+        return [self.stream[key][-1] for key in key_list]
+
+    def push(self,data_list):
+        for key,data in data_list:
+            self.stream[key].append(data)
+
+@ray.remote
+def environment(stream):
+    env = gym.make("MountainCarContinuous-v0")
+    s = env.reset()
+    for i in range(1000):
+        a = ray.get(ps.get.remote(["action"]))
+        s, r,done,info = env.step(a)
+        if done:
+            s = env.reset()
+        stream.push.remote([("state",s),("reward",r)])
+        time.sleep(1)
+
 
 ray.init()
 
-ps = ParameterServer.remote(10)
+ps = StreamServer.remote()
+ps.create_stream.remote([("state",0),("action",0.5),("reward",0)])
+environment.remote(ps)
+
+for i in range(100):
+    ps.push.remote([("action",np.random.random())])
+    print(ray.get(ps.get.remote(["state","action","reward"])))
+    time.sleep(1)
+#data = ps.get_stream.remote(["state","action","reward"])
+#print(data)
+#print(ps.get_stream.remote(["state","action","reward"]))
