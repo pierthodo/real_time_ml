68684bf4937d8440566bb2c1d35db916ddbcbb27
pierthodo <pierthodo@gmail.com>
2021-04-26 14:41:14 +0100

    Test commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Repo with my work on real time ML
+
+First we are going to experiment with 1 cpus for both learning and making decisions. 
+need to trade off learning and acting
+
--- scripts/ray_test_1.py
+++ scripts/ray_test_1.py
@@ -1,8 +1,24 @@
-
 import numpy as np
 import ray
 import gym
 import time
+import wandb
+
+
+@ray.remote
+class Agent(object):
+    def __init__(self,stream):
+        self.weights =  np.random.random(2).reshape((1,-1))
+        self.stream = stream
+
+    def run(self):
+        t0 = time.time()
+        timeout = 0
+        while not timeout:
+            state,timeout = ray.get(self.stream.get.remote(["state","timeout"]))
+            new_action = self.weights.dot(state)[0]
+            self.stream.push.remote([("action",new_action)])
+            time.sleep(0.5)
 
 @ray.remote
 class StreamServer(object):
@@ -13,37 +29,52 @@ class StreamServer(object):
         for key,data in data_list:
             self.stream[key] = [data]
 
-    def get(self,key_list):
-        return [self.stream[key][-1] for key in key_list]
+    def get(self,key_list,num_points=1):
+        if num_points == -1:
+            return [self.stream[key] for key in key_list]
+        elif num_points == 1:
+            return [self.stream[key][-1] for key in key_list]
+        else:
+            return [self.stream[key][-(num_points):] for key in key_list]
 
     def push(self,data_list):
         for key,data in data_list:
             self.stream[key].append(data)
 
+
 @ray.remote
-def environment(stream):
-    print("A")
+def environment(stream,time_limit=100):
+    t0 = time.time()
     env = gym.make("MountainCarContinuous-v0")
     s = env.reset()
-    for i in range(1000):
+    while (time.time() - t0) < time_limit:
         a = ray.get(ps.get.remote(["action"]))
         s, r,done,info = env.step(a)
         if done:
             s = env.reset()
         stream.push.remote([("state",s),("reward",r)])
-        time.sleep(1)
+        print("Env",ray.get(ps.get.remote(["state", "action", "reward"])))
+        print(time.time() - t0, "/",time_limit)
+        time.sleep(0.1)
+    stream.push.remote([("timeout",1)])
+
+wandb.init(project='real_time_ml', entity='pierthodo')
 
 
 ray.init()
 
 ps = StreamServer.remote()
-ps.create_stream.remote([("state",0),("action",0.5),("reward",0)])
-environment.remote(ps)
+env = gym.make("MountainCarContinuous-v0")
+s = env.reset()
+ps.create_stream.remote([("state", s), ("action", 0.5), ("reward", 0),("timeout",0)])
+ag = Agent.remote(ps)
 
-for i in range(100):
-    ps.push.remote([("action",np.random.random())])
-    print(ray.get(ps.get.remote(["state","action","reward"])))
-    time.sleep(1)
-#data = ps.get_stream.remote(["state","action","reward"])
+environment.remote(ps,time_limit=10)
+ray.get(ag.run.remote())
+
+data = ray.get(ps.get.remote(["state","action","reward"],num_points=-1))
+print(data[-1])
+result = {"reward":data[-1]}
+wandb.log(result)
 #print(data)
 #print(ps.get_stream.remote(["state","action","reward"]))
